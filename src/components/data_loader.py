4a7eff84341c1e1cd6af712a09e6a5d71cfbd1e8
RamNM <120415153+RamMahto1@users.noreply.github.com>
2025-11-27 23:45:20 +0400

    Update data_loader.py

--- src/components/data_loader.py
+++ src/components/data_loader.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-def load_iris_data(username='root', password='root', host='localhost', database='sales'):
+def load_iris_data(username='none', password='nonr', host='localhost', database='sales'):
     engine = create_engine(f"mysql+mysqlconnector://{username}:{password}@{host}/{database}")
     df = pd.read_sql("SELECT * FROM iris1", con=engine)
     return df
